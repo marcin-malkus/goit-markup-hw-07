b1127d333dac602ed33bddc431d066756a1da1ce
Marcin Małkus <60837508+marcin-malkus@users.noreply.github.com>
2022-05-15 17:45:55 +0200

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -3,7 +3,7 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    modalWindow: document.querySelector('[data-modal-box]'),
+    modalWindow: document.querySelector('[data-modal-window]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
